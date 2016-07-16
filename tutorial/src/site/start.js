ba6f3f50ca1692fee3fa936f8f5d48a7b74f97a3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-07-16 11:15:08 +0300

    Drop unfinished things

--- tutorial/src/site/start.js
+++
@@ -1,6 +0,0 @@
-import React    from "react"
-import ReactDOM from "react-dom"
-
-import Main from "./main"
-
-ReactDOM.render(<Main/>, document.getElementById("main"))
--- tutorial/test/tests.js
+++
@@ -1,4 +0,0 @@
-describe("dummy", () => {
-  it("succeeds", () => {
-  })
-})
